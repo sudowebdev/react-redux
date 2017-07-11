9f264f5a99ba0b6234a45ea16173b0ee0b816e11
Nikhil Arora <narora200@gmail.com>
2017-07-11 22:32:03 +0530

    reducer-todo.js created

---
+++ dev/js/reducers/reducer-todo.js
@@ -0,0 +1,33 @@
+export default function(){
+
+	return [
+		{
+			id: 1,
+			description: 'Wake up early.'
+		},
+		{
+			id: 2,
+			description: 'Go to bed early.'
+		},
+		{
+			id: 3,
+			description: 'Code, Eat, Sleep, Repeat.'
+		},
+		{
+			id: 4,
+			description: 'Learn new technologies.'
+		},
+		{
+			id: 5,
+			description: 'Commit everyday.'
+		},
+		{
+			id: 6,
+			description: 'Be consistent.'
+		},
+		{
+			id: 7,
+			description: 'Enjoy!!!!'
+		}
+	]
+}
